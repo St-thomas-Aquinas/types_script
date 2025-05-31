b13f7433810eb8fdf0d29754c338284f4214a3e7
St-thomas-Aquinas <maxkuria254@gmail.com>
2025-05-31 14:59:01 +0300

    Upload challenges 3,4,6

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -51,4 +51,45 @@ findwinner([{ name: "Alice", votes: 50 },
     { name: "Bob", votes: 75 },
     { name: "Charlie", votes: 65 },]);
 separator();
+function findlongest(fruits) {
+    console.log("Iam the function excuting challenge 4,longest word");
+    var len = fruits.length;
+    var longestword = " ";
+    var index = -1;
+    for (var i = 0; i < len; i++) {
+        var ret = fruits[i];
+        if (ret.length > longestword.length) {
+            longestword = ret;
+            index = i;
+        }
+    }
+    console.log(fruits[index]);
+}
+findlongest(["apple", "banana", "pear", "grapefruit"]);
+separator();
+function countProperties(obj) {
+    console.log("Iam the function excuting challenge 5,counting properties/keys");
+    var numberofkeys = Object.keys(obj);
+    var count = numberofkeys.length;
+    console.log("Number of keys/properties is ", count);
+}
+countProperties({ name: "Alice", age: 25, city: "Paris" });
+separator();
+//end
+//challenge 6: filterByLength
+function filterByLength(animals) {
+    console.log("Iam the function excuting challenge 6,filter by lenght");
+    var len = animals.length;
+    var minlen = 5;
+    var array = [];
+    for (var i = 0; i < len; i++) {
+        var ret = animals[i];
+        if (ret.length >= minlen) {
+            array.push(ret);
+        }
+    }
+    return array;
+}
+console.log(filterByLength(["cat", "giraffe", "hippo", "dog", "elephant"]));
+separator();
 //end
